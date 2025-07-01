4aac8fc379459fb06a7579ef8c4d47a8c511cd02
rodrigo <correo.rodrigo@gmail.com>
2025-07-01 18:24:30 -0500

    thinking

--- src/27.8_pointers.ts
+++ src/27.8_pointers.ts
@@ -8,12 +8,16 @@ export const sumZero = (arr1: number[], arr2: number[], arr3: number[]) : number
 
     if (arr1.length === arr2.length === arr3.length === 0) return res;
     
-    let i1 = arr1.length;
-    let i2 = arr2.length;
-    let i3 = arr3.length;
+    let i1 = 0;
+    let i2 = 0;
+    let i3 = 0;
+    let i = 0;
     
     let imax = Math.max(Math.max(i1, i2), i3);
-    for(let i = 0; i < imax; i++) {
+    while(i < imax) {
+	
+	// case 1. res is empty
+	if ()
 	
     }
 }
