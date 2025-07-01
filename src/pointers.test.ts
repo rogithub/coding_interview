809d758eb7af7d3a413677da765fa0a2a06062ab
rodrigo <correo.rodrigo@gmail.com>
2025-07-01 08:58:03 -0500

    first double pointers

--- src/pointers.test.ts
+++ src/pointers.test.ts
@@ -36,6 +36,53 @@ describe('Items in array doubled pointered', () => {
 	  expect(expected).toBe(true);
       });
   });
+
+
+  describe('different indices sum zero', () => {
+      it('should be true [-5, -3, -1, 0, 3, 4, 5]', () => {
+	  const arr = [-5, -3, -1, 0, 3, 4, 5];
+	  const expected = sumZero(arr);
+
+	  expect(expected).toBe(true);
+      });
+  });
+
+  describe('different indices sum zero', () => {
+      it('should be true [-5, 5]', () => {
+	  const arr = [-5, 5];
+	  const expected = sumZero(arr);
+
+	  expect(expected).toBe(true);
+      });
+  });
+
+  describe('different indices sum zero', () => {
+      it('should be false [1,2,3,4,5]', () => {
+	  const arr = [1,2,3,4,5];
+	  const expected = sumZero(arr);
+
+	  expect(expected).toBe(false);
+      });
+  });
+
+  describe('different indices sum zero', () => {
+      it('should be false [0]', () => {
+	  const arr = [0];
+	  const expected = sumZero(arr);
+
+	  expect(expected).toBe(false);
+      });
+  });
+
+  describe('different indices sum zero', () => {
+      it('should be false []', () => {
+	  const arr:number[] = [];
+	  const expected = sumZero(arr);
+
+	  expect(expected).toBe(false);
+      });
+  });
+
 });
 
 
