7f7c033e6f22b7fd83ae6e9d091a617b6ddf6588
rodrigo debian <correo.rodrigo@gmail.com>
2025-07-01 22:03:43 -0500

    going through

--- src/27.8_pointers.test.ts
+++ src/27.8_pointers.test.ts
@@ -22,4 +22,15 @@ describe("should be one sorted array", () => {
       expect(expected).toEqual([1, 2, 3]);
     });
   });
+
+  describe("3 arrays different lenghts", () => {
+    it("should be [0,1,2,3,4,5]", () => {
+      const arr1: number[] = [1, 2];
+      const arr2: number[] = [3, 3, 4, 5];
+      const arr3: number[] = [0, 1, 2, 2, 2];
+      const expected = toOneSorted(arr1, arr2, arr3);
+
+      expect(expected).toEqual([0, 1, 2, 3, 4, 5]);
+    });
+  });
 });
