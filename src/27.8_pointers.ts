d7d5f41f965bfa63ec2f87d6e4304fd98be0c425
rodrigo debian <correo.rodrigo@gmail.com>
2025-07-01 21:50:29 -0500

    going through

--- src/27.8_pointers.ts
+++ src/27.8_pointers.ts
@@ -1,8 +1,8 @@
 /**
  * Given 3 sorted arrays, return new array with the elements
- * of all 3 arrays, in sorted orderm, without duplicates
+ * of all 3 arrays, in sorted order, without duplicates
  **/
-export const sumZero = (
+export const toOneSorted = (
   arr1: number[],
   arr2: number[],
   arr3: number[],
@@ -21,7 +21,35 @@ export const sumZero = (
   let i3 = 0;
   let i = 0;
 
-  let imax = Math.max(Math.max(i1, i2), i3);
+  // 1 2
+  // 3 3 4 5
+  // 0 1 2 2 2
+
+  // 0 1 2 3 4 5
+  let candidates: number[];
+
+  do {
+    candidates = [];
+    if (i1 < arr1.length) {
+      candidates.push(arr1[i1]);
+    }
+    if (i2 < arr2.length) {
+      candidates.push(arr2[i2]);
+    }
+    if (i3 < arr3.length) {
+      candidates.push(arr3[i3]);
+    }
+
+    let min = Math.min(...candidates);
+
+    if (min === arr1[i1]) i1++;
+    if (min === arr2[i2]) i2++;
+    if (min === arr1[i3]) i3++;
+
+    if (res.length === 0 || res[res.length - 1] < min) {
+      res.push(min);
+    }
+  } while (candidates.length > 0);
 
   return res;
 };
