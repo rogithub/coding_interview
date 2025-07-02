15f0a22b1f7c43d40cb0bb40a56e3657b8f859b9
rodrigo <correo.rodrigo@gmail.com>
2025-07-02 15:17:35 -0500

    refactoring finding center

--- src/27.9_sortValleySharped.test.ts
+++ src/27.9_sortValleySharped.test.ts
@@ -2,7 +2,7 @@ import { sortValleyShaped } from "./27.9_sortValleySharped";
 
 describe("should be one sorted array", () => {
   describe("base cases", () => {
-    /*
+    
     it("should work for even length", () => {
       const arr: number[] = [8, 4, 2, 6];
       const expected = sortValleyShaped(arr);
@@ -53,13 +53,20 @@ describe("should be one sorted array", () => {
 
        expect(expected).toEqual([1,1,2,2]);
     });    
-    */
 
     it("shold work when the valley is not at the center", () => {
        const arr: number[] = [1,2];
        const expected = sortValleyShaped(arr);
 
        expect(expected).toEqual([1,2]);
+    });
+
+
+    it("finding center", () => {
+       const arr: number[] = [0, 1,2];
+       const expected = sortValleyShaped(arr);
+
+       expect(expected).toEqual([0,1,2]);
     });
 
 
--- src/27.9_sortValleySharped.ts
+++ src/27.9_sortValleySharped.ts
@@ -7,28 +7,26 @@
  * Given a valley-shaped array, arr, return a new array with the elements sorted.
  **/
 export const sortValleyShaped = (arr: number[]): number[] => {
-    const res: number[] = [];
-    if (arr.length === 0) return res;
-
-    // [8, 4, 2, 6]
-    // [2, 4, 6, 8]
-    let r = arr.length - 1;
-    let l = 0;
-    // center
-    if (arr.length % 2 === 0) {
-        let c = arr.length / 2;
-        l = c;
-        r = c + 1;        
-    } else {
-        let c = (arr.length - 1) / 2;
-        r = c; // set center to the right, we know it is at least this length
-        l = c - 1;
+    
+    if (arr.length <= 1) return arr;
+    if (arr.length == 2) {
+        return arr[0] < arr[1] ? arr : [arr[1], arr[0]];
     }
+    
+    const res: number[] = [];
+    // find center
+    let l = 0;
+    let r = 1;
+    while(arr[l] >= arr[r] && r < arr.length) {
+        l++;
+        r++;
+    }
+
+    //console.log(`${arr} center L${l} R${r}`)
 
 
     while(l > -1 || r < arr.length) {
         if (l > -1 && r < arr.length) {
-            console.log("CASE 0");
             if (arr[l] > arr[r]) {
                 res.push(arr[r]);
                 r++;                
@@ -36,26 +34,23 @@ export const sortValleyShaped = (arr: number[]): number[] => {
                 res.push(arr[l]);
                 l--;
             }
-            console.log("RES", res);
             continue;
         }
 
         if (l > -1) {
-            console.log("CASE 1");
             res.push(arr[l]);
             l--;
-            console.log("RES", res);
         }
 
         if (r < arr.length) {
-            console.log("CASE 2");
             res.push(arr[r]);
             r++;  
         }       
     }
 
-    console.log(arr);
-    console.log(res);
     
     return res;
 };
+
+
+
